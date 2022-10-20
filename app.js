4ac6d04ce0348366717a344228c4d166bc11b876
Alex <rengarsanj@gmail.com>
2022-10-19 22:19:29 -0800

    testing

--- app.js
+++ app.js
@@ -3,23 +3,39 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/pets');
 
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
 
 
 
-const Owner = mongoose.model('Owner', { name: String });
-
-const ownername = new Owner({ name: 'Eduardo' });
-
-const BredPit = new Owner({ name: 'BredPit' });
-
-const Jonifer = new Owner({ name: 'Jonifer' });
-
-const Anna = new Owner({ name: 'Anna' });
 
 
+
+const Cat = mongoose.model('Cat', { 
+	name: {
+		type: String,
+		required: [true, "Please enter name"]
+	},
+	rating: {
+		type: Number, 
+		min: 1,
+		max: 10
+	}
+	});
+const kitty = new Cat({ name: 'Mjau'});
+
+const dog = new Cat({
+	name: "Doggu",
+	rating: 8
+});
+
+const Owner = mongoose.model('Owner', { name: String, favouritePet: Cat});
+const ownername = new Owner({ name: 'Amy', favouritePet: dog});
+
+
+
+
+
+ownername.save();
+// dog.save();
 // kitty.save();
 // ownername.save();
 // Owner.insertMany([BredPit, Jonifer, Anna], function(err) {
@@ -31,45 +47,29 @@ const Anna = new Owner({ name: 'Anna' });
 // });
 
 Cat.find(function(err, cats) {
-    if (err) {
-        console.log(err);
-    } else {
-        cats.forEach(function(Cat) {
-            const catName = Cat.name ;
-            console.log(catName);
-        });
-    }
+	if (err) {
+		console.log(err);
+	} else {
+		// mongoose.connection.close();
+		cats.forEach(function(Cat) {
+			const catName = Cat.name ;
+			console.log(catName);
+		});
+	}
 });
 
+// Cat.updateOne({_id: "63505461aba2415f455fd65f"}, {name: "GoodCat"}, function(err) {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log("Succesfully updated the document! ");
+// 	}
+// });
 
 
+// Cat.deleteOne({_id: "63505461aba2415f455fd65f"}, function(err) {});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Cat.deleteMany({name: "Zildjian"}, function(err) {});
 
 
 
